bdef54190f5f3db3f79694e155916c77521b08b0
intrnl <intrnl28+git@gmail.com>
2020-08-24 05:26:19 +0700

    substate-preact: remove manual types

--- packages/substate-preact/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Store } from '@intrnl/substate';
-
-export function useStoreState<S> (store: Store<S>): S
-export function useStoreState<S, T> (
-	store: Store<S>,
-	selector: ((store: S) => T),
-	deps?: ReadonlyArray<any>
-): T
