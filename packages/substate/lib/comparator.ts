a15d0e97af9f8c7c20e70fdf9d646aa81e3485cc
intrnl <intrnl28+git@gmail.com>
2020-08-24 07:49:44 +0700

    substate: consistent Object.is usage

--- packages/substate/lib/comparator.ts
+++ packages/substate/lib/comparator.ts
@@ -1,15 +1,17 @@
+let is = Object.is;
+
 export function shallow (a: any, b: any): boolean {
-	if (Object.is(a, b)) return true;
+	if (is(a, b)) return true;
 
 	if (!is_object(a) || !is_object(b)) return false;
 	if (get_keys_length(a) !== get_keys_length(b)) return false;
 
 	for (let key in a) {
-		if (a[key] !== b[key]) return false;
+		if (!is(a[key], b[key])) return false;
 	}
 
 	for (let key in b) {
-		if (a[key] !== b[key]) return false;
+		if (!is(a[key], b[key])) return false;
 	}
 
 	return true;
